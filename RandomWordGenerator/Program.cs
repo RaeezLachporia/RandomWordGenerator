a4fb724be01d3417466e2bdd7b0addd7abcc3007
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2022-07-27 22:58:13 +0200

    Add project files.

---
+++ RandomWordGenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+namespace RandomWordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number of words you would like to generate");
+            int NumWords = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(NumWords);
+            String FileName = "WordBank.txt";
+            int[] myNumbers = File.ReadLines(FileName).Select(line => int.Parse(line)).ToArray();
+
+            for (int i = 0; i < NumWords; i++)
+            {
+                Random r = new Random();
+                int rIndex = r.Next(FileName.Length);
+                Console.WriteLine("your random words are: " + FileName[rIndex]);
+            }
+        }
+
+       
+    }
+}
